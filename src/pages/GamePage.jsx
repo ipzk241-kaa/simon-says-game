22fe24c12f5d8adc1e9f6c17681151866d7f9313
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-10-29 19:30:04 +0200

    refactor: clean up GamePage by moving logic to custom hook

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,16 +1,36 @@
 import Header from "../components/Header/Header";
 import ButtonColor from "../components/ButtonColor/ButtonColor";
+import { useSimonGame } from "../hooks/useSimonGame";
 
 export default function GamePage() {
+  const {
+    level,
+    activeColor,
+    isGameOver,
+    handlePlayerClick,
+    startGame,
+  } = useSimonGame();
+
   return (
-    <>
-      <Header title="Рівень 1" />
+    <div className="game-page">
+      <Header title={isGameOver ? "Гру завершено" : `Рівень ${level}`} />
+
       <div className="game-board">
-        <ButtonColor color="red" />
-        <ButtonColor color="green" />
-        <ButtonColor color="blue" />
-        <ButtonColor color="yellow" />
+        {["red", "green", "blue", "yellow"].map((color) => (
+          <ButtonColor
+            key={color}
+            color={color}
+            isActive={activeColor === color}
+            onClick={() => handlePlayerClick(color)}
+          />
+        ))}
       </div>
-    </>
+
+      {isGameOver && (
+        <button onClick={startGame} className="restart-btn">
+          Почати знову
+        </button>
+      )}
+    </div>
   );
 }

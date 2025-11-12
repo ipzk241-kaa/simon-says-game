4bd398fe8cab2514581c158822cf93b8fec0abdd
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-11-12 17:00:34 +0200

    feat: implement settings and GameOver portal

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import { GameSettingsProvider } from "./context/GameSettingsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <GameSettingsProvider>
+    <App />
+  </GameSettingsProvider>);

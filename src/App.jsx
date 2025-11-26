8d012fcdc7dae79f2a9490d20430ba57d7478e74
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-11-26 18:33:50 +0200

    feat: add dynamic userId routes and base navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
 import StartPage from "./pages/StartPage";
 import GamePage from "./pages/GamePage";
 import ResultPage from "./pages/ResultPage";
 
 function App() {
-  const [page, setPage] = useState("start");
-  const [score, setScore] = useState(0);
-
   return (
-    <>
-      {page === "start" && <StartPage onStart={() => setPage("game")} />}
-      {page === "game" && (
-        <GamePage onGameOver={(lvl) => { setScore(lvl); setPage("result"); }} />
-      )}
-      {page === "result" && (
-        <ResultPage score={score} onRestart={() => setPage("start")} />
-      )}
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/start" replace />} />
+      <Route path="/start" element={<StartPage />} />
+      <Route path="/user/:userId/game" element={<GamePage />} />
+      <Route path="/user/:userId/result" element={<ResultPage />} />
+      <Route path="*" element={<Navigate to="/start" replace />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { GameSettingsProvider } from "./context/GameSettingsContext";
+import { UserProvider } from "./context/UserContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <GameSettingsProvider>
-    <App />
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
   </GameSettingsProvider>);
